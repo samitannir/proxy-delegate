fd2a5502f92b7c2c87752b02184bbb82df7bf386
Sami Tannir <samitannir@gmail.com>
2020-03-10 22:12:10 -0400

    FIrst Commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,12 @@
+const ProxyDelegate = artifacts.require('ProxyDelegate');
+const ProxyCall = artifacts.require('ProxyCall');
+const SomeLibrary = artifacts.require('SomeLibrary');
+
+module.exports = function(deployer) {
+    deployer.deploy(SomeLibrary)
+    .then(() => deployer.deploy(ProxyDelegate, SomeLibrary.address));
+
+    deployer.deploy(SomeLibrary)
+    .then(() => deployer.deploy(ProxyCall, SomeLibrary.address));
+
+}
